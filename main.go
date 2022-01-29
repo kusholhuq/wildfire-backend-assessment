36b64aa3fcf831c88e1c5a67b3b3038ccdf79d29
Mohsinul Huq <kusholhuq@gmail.com>
2022-01-28 19:03:27 -0800

    defined types

--- main.go
+++ main.go
@@ -8,3 +8,19 @@ import(
 )
 
 var client *http.Client
+
+type Name struct {
+	FirstName string `json:"first_name"`
+	LastName string	`json:"last_name"`
+}
+
+type Response struct {
+	Type string `json:"type"`
+	Value ResponseValue `json:"value"`
+}
+
+type ResponseValue struct {
+	Id int
+	Joke string
+	Categories []string
+}
